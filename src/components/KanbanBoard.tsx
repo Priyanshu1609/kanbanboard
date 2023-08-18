a7cc2f7f7dd549d773e015cf7e0d017859841265
Priyanshu Kumar Panda <79529908+Priyanshu1609@users.noreply.github.com>
2023-08-19 01:19:30 +0530

    ui/changes

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -4,17 +4,39 @@ import ColumnContainer from "./ColumnContainer";
 import { Ticket, User } from "../types";
 import "./KanbanBoard.css";
 import useFetchData from "../hooks/useFetchData";
-import { priorityColumn, statusColumn } from "../contants";
+import { priorityColumn, statusColumn, userIdColumn } from "../contants";
 
 type Props = {
   group: "status" | "priority" | "user";
   sort: "priority" | "title";
 };
 
-function KanbanBoard(props: Props) {
+interface SortType {
+  sort: "priority" | "title";
+}
+
+// Function to sort tickets by title in ascending order
+function sortTicketsByTitleAscending(tickets: Ticket[]): Ticket[] {
+  return tickets.slice().sort((a, b) => a.title.localeCompare(b.title));
+}
+
+function KanbanBoard({ group, sort }: Props) {
   const useFetchHook = useFetchData();
   const [tickets, setTickets] = useState<Ticket[]>([]);
   const [users, setUsers] = useState<User[]>([]);
+
+  // Function to sort tickets by priority in descending order
+  const sortTickets = (
+    tickets: Ticket[],
+    sort: "priority" | "title"
+  ): Ticket[] => {
+    if (sort === "title") {
+      return tickets.slice().sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sort === "priority") {
+      return tickets.slice().sort((a, b) => b.priority - a.priority);
+    }
+    return tickets; // Default case if sort parameter is not "title" or "priority"
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -32,14 +54,39 @@ function KanbanBoard(props: Props) {
     <div className="kanban-container">
       <div className="kanban-column-wrapper">
         {/* <div className="kanban-column-container"> */}
-          {priorityColumn?.map((col) => (
+        {group === "priority" &&
+          priorityColumn?.map((col) => (
             <ColumnContainer
               key={col.id}
               column={col}
-              tasks={tickets.filter((ticket) => ticket.priority === col.id)}
+              tasks={sortTickets(
+                tickets.filter((ticket) => ticket.priority === col.id),
+                sort
+              )}
             />
           ))}
-        {/* </div> */}
+        {group === "status" &&
+          statusColumn?.map((col) => (
+            <ColumnContainer
+              key={col.id}
+              column={col}
+              tasks={sortTickets(
+                tickets.filter((ticket) => ticket.status === col.id),
+                sort
+              )}
+            />
+          ))}
+        {group === "user" &&
+          userIdColumn?.map((col) => (
+            <ColumnContainer
+              key={col.id}
+              column={col}
+              tasks={sortTickets(
+                tickets.filter((ticket) => ticket.userId === col.id),
+                sort
+              )}
+            />
+          ))}
       </div>
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./NavBar.css";
 import { VscSettings } from "react-icons/vsc";
-import { BiChevronDown } from "react-icons/bi";
-import DropDown from "./DropDown";
+import { BiChevronRight, BiChevronDown } from "react-icons/bi";
 
 type Props = {
   group: "status" | "priority" | "user";
@@ -11,21 +10,39 @@ type Props = {
   setSort: (sort: "priority" | "title") => void;
 };
 
-const Group = () => (
-  <button className="navbar-selection-btn">
-    <p style={{ fontWeight: "500" }}>Status</p>
-    <BiChevronDown className="nav-option-icon" />
-  </button>
-);
-
-const Order = () => (
-  <button className="navbar-selection-btn">
-    <p style={{ fontWeight: "500" }}>Priority</p>
-    <BiChevronDown className="nav-option-icon" />
-  </button>
-);
-
 const NavBar = ({ group, setGroup, sort, setSort }: Props) => {
+  const Group = () => (
+    <button className="navbar-selection-btn" id="btn-1">
+      <p style={{ fontWeight: "500", textTransform: "capitalize" }}>{group}</p>
+      <BiChevronRight className="nav-option-icon" />
+      <div className="navbar-second-select">
+        <p onClick={() => setGroup("status")} className="">
+          Status
+        </p>
+        <p onClick={() => setGroup("user")} className="">
+          User
+        </p>
+        <p onClick={() => setGroup("priority")} className="">
+          Priority
+        </p>
+      </div>
+    </button>
+  );
+
+  const Order = () => (
+    <button className="navbar-selection-btn" id="btn-2">
+      <p style={{ fontWeight: "500", textTransform: "capitalize" }}>{sort}</p>
+      <BiChevronRight className="nav-option-icon" />
+      <div className="navbar-second-select">
+        <p onClick={() => setSort("priority")} className="">
+          Priority
+        </p>
+        <p onClick={() => setSort("title")} className="">
+          Title
+        </p>
+      </div>
+    </button>
+  );
   return (
     <div className="navbar-container">
       <button className="nav-btn">
